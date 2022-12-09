b0d03cfb5673cd229fe0a505b7589f2f5781e7e4
fox wei <3154527910qq.com>
2022-12-09 16:16:04 +0800

    modify id

--- life_revive/model/meItem.go
+++ life_revive/model/meItem.go
@@ -2,7 +2,7 @@ package model
 
 //*我的 页面配置项
 type MeItem struct {
-	ItemId string `json:"marketId"`
+	ItemId string `json:"itemId"`
 	Src    string `json:"src"`
 	Title  string `json:"title"`
 	Type   string `json:"type"`
